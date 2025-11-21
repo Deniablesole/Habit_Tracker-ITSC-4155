5f5fef0985b22ac2401d94b21ce78b754f70addc
Mohamed-Bounouail <mbounoua@charlotte.edu>
2025-11-21 01:29:58 -0500

    Revert "Update habit-web UI and fix build issues"
    This reverts commit 1fa5155e2635a35b1605c01d916a6939d6afd75e.

--- habit-web/src/api/client.js
+++ habit-web/src/api/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"; // for local dev
+
 export const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || "/api",
-  baseURL: "/api", 
+  baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
 });
